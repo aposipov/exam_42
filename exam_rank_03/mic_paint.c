2af8cf5735ac14b86c5cffcd38e1568586fc5e67
aposipov <a.p.osipov@ro.ru>
2022-03-08 10:50:39 +0300

    03

--- exam_rank_03/mic_paint.c
+++ exam_rank_03/mic_paint.c
@@ -13,11 +13,12 @@ typedef struct draw {
 	float x, y, w, h;
 }   Draw;
 
-int error_msg(FILE *fd, int err){
+int error_msg(FILE *fd, int err)
+{
 	if (err == 2)
 		write(1, "Error: Operation file corrupted\n", 32);
 	else if (err == 1)
-		write(1, "Error rgument\n", 16);
+		write(1, "Error argument\n", 16);
 	else{
 		for(int i = 0; i < h; i++){
 			write(1, tab[i], w);
@@ -31,7 +32,8 @@ int error_msg(FILE *fd, int err){
 
 int in_rectangle(float x, float y, Draw *el)
 {
-	if (((x < el->x) || (el->x +el->w) < x) || (y <el->y) || (el->y + el->h < y))
+	if (((x < el->x) || (el->x + el->w) < x) || (y < el->y) || (el->y + el->h
+	        < y))
 		return (0);
 	if (x - el->x < 1.000 || (el->x + el->w) - x < 1.000 || (y - el->y < 1.0000) || (el->y + el->h) - y < 1.0000)
 		return (2);
@@ -47,14 +49,20 @@ int main (int ac, char **av)
 	fd = NULL;
 	if (ac != 2)
 		return (error_msg(fd, 1));
-	if ((fd = fopen(av[1], "r"))){
-		if ((res = fscanf(fd,"%d %d %c", &w, &h, &bg)) ==3){
-			if (w> 0 && w <= 300 && h > 0 && h <= 300){
+	if ((fd = fopen(av[1], "r")))
+	{
+		if ((res = fscanf(fd,"%d %d %c", &w, &h, &bg)) ==3)
+		{
+			if (w> 0 && w <= 300 && h > 0 && h <= 300)
+			{
 				tab = malloc(h * sizeof(char *));
-				for (int i = 0; i < h; i++){
+				for (int i = 0; i < h; i++)
+				{
 					tab[i] = malloc(w * sizeof(char));
 					memset(tab[i], bg, w);
-				}while(1){
+				}
+				while(1)
+				{
 					res = fscanf(fd,"\n%c %f %f %f %f %c",&el.t, &el.x, &el.y, &el.w, &el.h, &el.c);
 					if (res == -1)
 						return (error_msg(fd, 0));
--- exam_rank_03/min_paint.c
+++ exam_rank_03/min_paint.c
@@ -38,24 +38,31 @@ int main(int ac, char **av)
 	if (ac != 2)
 		return (error_msg(fd,1));
 	if ((fd = fopen(av[1],"r"))){
-		if ((res = fscanf(fd, "%d %d %c", &w,&h,&bg)) == 3){
-			if (w > 0 && w <= 300 && h > 0 && h <= 300){
+		if ((res = fscanf(fd, "%d %d %c", &w,&h,&bg)) == 3)
+		{
+			if (w > 0 && w <= 300 && h > 0 && h <= 300)
+			{
 				tab = malloc(h * sizeof(char *));
 				for (int i = 0; i < h; i++){
 					tab[i] = malloc(w * sizeof(char));
 					memset(tab[i], bg, w);
 				}
-				while (1){
+				while (1)
+				{
 					res = fscanf(fd, "\n%c %f %f %f %c",&el.t,&el.x, &el.y, &el.r, &el.c);
 					if (res == -1)
 						return (error_msg(fd,0));
 					else if (res != 5 || el.r <= 0 || (el.t != 'c' && el.t != 'C'))
 						break;
-					for (int line = 0;line < h; line ++){
-						for (int col = 0; col < w; col++){
-							sqr = sqrtf((col - el.x)*(col -el.x) + (line -el.y)*(line -el.y));
-							if (sqr<el.r){
-								if (el.t == 'c' && sqr+1>el.r)
+					for (int line = 0;line < h; line ++)
+					{
+						for (int col = 0; col < w; col++)
+						{
+							sqr = sqrtf((col - el.x) * (col - el.x) + (line -el
+									.y)*(line -el.y));
+							if (sqr < el.r)
+							{
+								if (el.t == 'c' && sqr + 1 > el.r)
 									tab[line][col] = el.c;
 								else if (el.t == 'C')
 									tab[line][col] = el.c;
