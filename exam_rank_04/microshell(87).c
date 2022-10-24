cbbfa4e0bec9db423dcbaca5ab6200c39aca6d8f
Alex Osipov <69660163+aposipov@users.noreply.github.com>
2022-10-24 22:09:14 +1000

    Create microshell(87).c

---
+++ exam_rank_04/microshell(87).c
@@ -0,0 +1,90 @@
+https://github.com/Abzwingten/ECOLE42_EXAMS/blob/master/EXAM_03/microshell.c
+cyelena
+
+#include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
+
+int  ft_putstr_fd2(char *str, char *arg)
+{
+  while (*str)
+    write(2, str++, 1);
+  if (arg)
+    while (*arg)
+      write(2, arg++, 1);
+  write(2, "\n", 1);
+  return (1);
+}
+
+int  ft_execute(char *argv[], int i, int tmp_fd, char *env[])
+{
+  argv[i] = NULL;
+  dup2(tmp_fd, STDIN_FILENO);
+  close(tmp_fd);
+  execve(argv[0], argv, env);
+  return (ft_putstr_fd2("error: cannot execute ", argv[0]));
+}
+
+int  main(int argc, char *argv[], char *env[])
+{
+  int  i;
+  int pid;
+  int fd[2];
+  int tmp_fd;
+  (void)argc;
+
+  pid = 0;
+  i = 0;
+  tmp_fd = dup(STDIN_FILENO);
+  while (argv[i] && argv[i + 1])
+  {
+    argv = &argv[i + 1];
+    i = 0;
+    while (argv[i] && strcmp(argv[i], ";") && strcmp(argv[i], "|"))
+      i++;
+    if (strcmp(argv[0], "cd") == 0) //cd
+    {
+      if (i != 2)
+        ft_putstr_fd2("error: cd: bad arguments", NULL);
+      else if (chdir(argv[1]) != 0)
+        ft_putstr_fd2("error: cd: cannot change directory to ", argv[1]  );
+    }
+    else if (i != 0 && (argv[i] == NULL || strcmp(argv[i], ";") == 0)) //exec in stdout
+    {
+      pid = fork();
+      if ( pid == 0)
+      {
+        if (ft_execute(argv, i, tmp_fd, env))
+          return (1);
+      }
+      else
+      {
+        close(tmp_fd);
+        while (waitpid(-1, NULL, WUNTRACED) != -1)
+          ;
+        tmp_fd = dup(STDIN_FILENO);
+      }
+    }
+    else if(i != 0 && strcmp(argv[i], "|") == 0)
+    {
+      pipe(fd);
+      pid = fork();
+      if (pid == 0)
+      {
+        dup2(fd[1], STDOUT_FILENO);
+        close(fd[0]);
+        close(fd[1]);
+        if (ft_execute(argv, i, tmp_fd, env))
+          return (1);
+      }
+      else
+      {
+        close(fd[1]);
+        close(tmp_fd);
+        tmp_fd = fd[0];
+      }
+    }
+  }
+  close(tmp_fd);
+  return (0);
+}
