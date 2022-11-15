4499427f8c9c681e467219ea45be8b1559d89e8c
Alex Osipov <69660163+aposipov@users.noreply.github.com>
2022-11-15 16:39:11 +0300

    Create Fwoosh.hpp

---
+++ exam_rank_05/cpp_module_02/Fwoosh.hpp
@@ -0,0 +1,11 @@
+
+#include "ASpell.hpp"
+
+class Fwoosh : public ASpell {
+public:
+    Fwoosh() : ASpell("Fwoosh", "fwooshed") {}
+    ~Fwoosh() {}
+    virtual ASpell* clone() const {
+        return new Fwoosh();
+    }
+};
