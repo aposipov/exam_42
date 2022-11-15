d4386100bdbdca16e4c2cd15bf7d14ff5f6ed560
Alex Osipov <69660163+aposipov@users.noreply.github.com>
2022-11-15 16:35:51 +0300

    Create ASpell.hpp

---
+++ exam_rank_05/cpp_module_02/ASpell.hpp
@@ -0,0 +1,25 @@
+
+#pragma once
+#include <iostream>
+#include "ATarget.hpp"
+class ATarget;
+
+class ASpell {
+public:
+    ASpell() {} // 
+    ASpell(std::string name, std::string effects) {
+        _name = name;
+        _effects = effects;    
+    }
+    virtual ~ASpell() {}
+
+    std::string getName() { return _name; }
+    std::string getEffects() const { return _effects; }
+
+    void launch(const ATarget &target);
+    virtual ASpell* clone() const = 0;
+
+private:
+    std::string _name;
+    std::string _effects;
+};
