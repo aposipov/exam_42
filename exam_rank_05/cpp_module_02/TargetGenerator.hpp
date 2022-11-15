bc119f35b53939cfa79b5d3da6152fc66f049e50
Alex Osipov <69660163+aposipov@users.noreply.github.com>
2022-11-15 16:40:32 +0300

    Create TargetGenerator.hpp

---
+++ exam_rank_05/cpp_module_02/TargetGenerator.hpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+#include <map>
+#include "ATarget.hpp"
+
+class TargetGenerator {
+public:
+    TargetGenerator() {}
+    ~TargetGenerator() {}
+
+    void learnTargetType(ATarget* target) {
+        tar_map[target->getType()] = target;
+    }
+
+    void forgetTargetType(std::string const &targetType) {
+        tar_map.erase(targetType);
+    }
+
+    ATarget* createTarget(std::string const &targetType) {
+        if (tar_map[targetType] != 0)
+            return tar_map[targetType];
+        return 0;
+    }
+private:
+    std::map<std::string, ATarget*> tar_map;
+};
