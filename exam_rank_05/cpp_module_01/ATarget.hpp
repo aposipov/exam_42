cf2c0397c6411eada1738b78a07d21cfd84fc8c4
Alex Osipov <69660163+aposipov@users.noreply.github.com>
2022-11-15 13:50:59 +0300

    Create ATarget.hpp

---
+++ exam_rank_05/cpp_module_01/ATarget.hpp
@@ -0,0 +1,22 @@
+
+#pragma once
+#include <iostream>
+#include "ASpell.hpp"
+class ASpell;
+
+class ATarget {
+public:
+    ATarget() {} // 
+    ATarget(std::string type) {
+        _type = type;  
+    }
+    virtual ~ATarget() {}
+
+    std::string getType() { return _type; }
+
+    void getHitBySpell(const ASpell &spell) const;
+    virtual ATarget* clone() const = 0;
+    
+private:
+    std::string _type;
+};
