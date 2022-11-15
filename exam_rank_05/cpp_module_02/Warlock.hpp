8d05ed1b91c4bef3afdebe7252d781f944b874dd
Alex Osipov <69660163+aposipov@users.noreply.github.com>
2022-11-15 16:34:22 +0300

    Update Warlock.hpp

--- exam_rank_05/cpp_module_02/Warlock.hpp
+++ exam_rank_05/cpp_module_02/Warlock.hpp
@@ -1,1 +1,51 @@
 
+#include <iostream>
+// #include <map>
+#include "ASpell.hpp"
+#include "ATarget.hpp"
+#include "SpellBook.hpp"
+
+class Warlock {
+public:
+    Warlock(std::string name, std::string title) {
+        _name = name;
+        _title = title;
+        std::cout << _name << ": This looks like another boring day." << std::endl;
+    }
+
+    ~Warlock() {
+        std::cout << _name << ": My job here is done!" << std::endl;
+    }
+
+    const std::string &getName() const { return _name; }
+    const std::string &getTitle() const { return _title; }
+    void setTitle(const std::string &title) { _title = title ;}
+
+    void introduce() const {
+        std::cout << _name << ": I am " << _name << ", " << _title << "!" << std::endl;
+    }
+
+    void learnSpell(ASpell* spell) {
+        // map[spell->getName()] = spell;
+        book.learnSpell(spell);
+    }
+
+    void forgetSpell(std::string spellName) {
+        // map.erase(spellName);
+        book.forgetSpell(spellName);
+    }
+
+    void launchSpell(std::string spellName, ATarget &target) {
+        // if (map[spellName] != 0)
+            // map[spellName]->launch(target);
+        ASpell* tmp = book.createSpell(spellName);
+        if (tmp)
+            tmp->launch(target);
+    }
+
+private:
+    std::string _name;
+    std::string _title;
+    // std::map<std::string, ASpell*> map;
+    SpellBook book;
+};
